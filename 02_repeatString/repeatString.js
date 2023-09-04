ae63d6b094d0c7395fc21f6512add609dce1b9f6
Allan <allanchiang24@gmail.com>
2023-09-04 16:35:26 +1200

    Add generateRandomNumber function with math.floor and math.random

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,9 @@
+const generateRandomNumber = function(num) {
+    let num = Math.floor(Math.random() * 1000);
+}
+
 const repeatString = function() {
-    let string = '';
-
-    for(let i = 0; i < 1; i++) {
-        string += 'ERROR';
-    }
-
-    return string;
+    
 };
 
 // Do not edit below this line
