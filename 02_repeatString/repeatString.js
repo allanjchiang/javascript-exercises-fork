7e1aaffe4952b5666eac6c4e343c7aaae94b40c4
Allan <allanchiang24@gmail.com>
2023-09-04 17:42:16 +1200

    Add loop to print hey a random number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,18 @@
 let randomNum;
+let string;
+let num;
 
 function generateRandomNumber() {
     randomNum = Math.floor(Math.random() * 1000);
+    return randomNum;
 }
 
 function repeatString() {
-    num = generateRandomNumber();
-    return num;
+    for(let i = 0; i < generateRandomNumber(); i++) {
+        string += "hey";
+    }
+
+    return string;
 }
 
 // Do not edit below this line
