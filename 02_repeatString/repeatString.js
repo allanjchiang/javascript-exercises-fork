4bd8091d7b25d04e5db7535f212e6ae909d3b7f8
Allan <allanchiang24@gmail.com>
2023-09-04 18:16:23 +1200

    Declare empty string variable inside function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,12 @@
-let string = "";
-
 function repeatString(hey, num) {
     if (num < 0) return "ERROR";
+
+    let string = "";
+
     for(let i = 0; i < num; i++) {
         string += hey;
     }
+    
     return string;
 }
 
