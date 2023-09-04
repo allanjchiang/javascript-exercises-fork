9411378554eaaafcb60d965ea9eec709a92ad5bb
Allan <allanchiang24@gmail.com>
2023-09-04 16:22:58 +1200

    Change loop condition to <1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function() {
     let string = '';
 
-    for(let i = 0; i < 0; i++) {
+    for(let i = 0; i < 1; i++) {
         string += 'ERROR';
         string += -1;
     }
