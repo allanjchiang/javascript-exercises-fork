9f0bcd05787742e007e6384bc595710a9808ea25
Allan <allanchiang24@gmail.com>
2023-09-04 17:35:22 +1200

    Declare variable num outside function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,11 @@
-generateRandomNumber(num) {
+let num;
+
+function generateRandomNumber(num) {
     let num = Math.floor(Math.random() * 1000);
 }
 
 function repeatString() {
-    
+
 }
 
 // Do not edit below this line
