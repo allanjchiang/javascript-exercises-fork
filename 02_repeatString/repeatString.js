690235f9a2ddacba577df659235751d7b8a58943
Allan <allanchiang24@gmail.com>
2023-09-04 16:19:14 +1200

    Concatenate -1 to num and return it

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -4,9 +4,11 @@ const repeatString = function() {
 
     for(let i = 0; i < 0; i++) {
         string += 'ERROR';
+        num += -1;
     }
 
     return string;
+    return num;
 };
 
 // Do not edit below this line
