aebb64b3ba38d2bf11dbe2bbeec6a885e58fc2f9
Allan Chiang <allanchiang24@gmail.com>
2023-09-05 17:57:58 +1200

    Return reversed words

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
 const reverseString = function(words) {
     let splitWords = words.split();
     let reversedWords = splitWords.reverse();
+    return reversedWords;
 };
 
 // Do not edit below this line
