2feb89f3ff58db103c41499ca10856f5c33e97a2
Allan Chiang <allanchiang24@gmail.com>
2023-09-10 16:47:25 +1200

    Add for loop which takes arguments[i] and arguments[i].length

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function(arr) {
     let values = "";
+
+    for(let i = 1; i < arguments.length; i++) {
+        arr.splice(arguments[i], arguments[i].length);
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
