a6eb0bc5d6449be15fce6b2eff10a1bac0b9cf34
Allan Chiang <allanchiang24@gmail.com>
2023-09-10 18:12:29 +1200

    Add forEach loop that takes all non ...args and pushes them to a new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,12 @@
-const removeFromArray = function(arr, ...manyMoreArgs) {
-    const remove = manyMoreArgs;
+const removeFromArray = function(arr, ...args) {
+    const newArray = [];
 
-    for(let i = remove.length; i >= 0; i--){
-        arr.splice(remove[i], 1);
-    }
-
-    return arr;
+    arr.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
