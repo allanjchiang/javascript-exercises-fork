9300be8e15d7793dcb8790b9b6ac4b58029860d0
Allan Chiang <allanchiang24@gmail.com>
2023-09-11 18:04:47 +1200

    Add if statement for if year is divisible by 100

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,16 @@
 const leapYears = function(year) {
-    if (year % 4 === 0 && year % 400 === 0) {
-        return true;
-    } else if (year % 4 !== 0) {
-        return false;
-    } else if (year % 4 === 0) {
-        return true;
+    if (year % 100 === 0) {
+        if (year % 4 === 0 && year % 400 !== 0) {
+            return false;
+        } else if (year % 4 === 0 && year % 400 === 0) {
+            return true;
+        }
+    } else if (year % 100 !== 0) {
+        if (year % 4 === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 
