1786d53e82081b9e8a104e45550f3da3bd4de395
Allan Chiang <allanchiang24@gmail.com>
2023-09-11 17:32:24 +1200

    Add two if statements to determine which parameter in the function is bigger

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,14 @@
 const sumAll = function(min, max) {
     let summedNum = 0;
-    for(let i = 0; i <= max; i++) {
-        summedNum += i;
-    }
+    if (max > min) {
+        for(let i = 0; i <= max; i++) {
+            summedNum += i;
+        }
+    } else if (min > max) {
+        for(let i = 0; i <= min; i++){
+            summedNum += i;
+        }
+    }    
     return summedNum;
 };
 
