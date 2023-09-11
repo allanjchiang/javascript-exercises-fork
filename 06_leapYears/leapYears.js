30651af3dc6a9051ea80fca66e5c1add8ca86036
Allan Chiang <allanchiang24@gmail.com>
2023-09-11 17:58:34 +1200

    Change order of if statements so !== 0 remainder is above === 0

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,10 @@
 const leapYears = function(year) {
     if (year % 4 === 0 && year % 400 === 0) {
         return true;
+    } else if (year % 4 !== 0) {
+        return false;
     } else if (year % 4 === 0) {
         return true;
-    } else {
-        return false;
     }
 };
 
