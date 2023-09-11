543dd93b48fdee57373fab08a4420210c213a1c1
Allan Chiang <allanchiang24@gmail.com>
2023-09-11 16:59:53 +1200

    Create for loop that sums all numbers from min to max

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let summedNum;
+    for(let i = 0; i <= max; i++) {
+        summedNum += i;
+    }
+    return summedNum;
 };
 
 // Do not edit below this line
