bc3fffdace00fa04aad277002685e75010632f47
Allan Chiang <allanchiang24@gmail.com>
2023-09-11 17:40:39 +1200

    Add if statement that checks if parameter is a string

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function(min, max) {
     let summedNum = 0;
-    if (min < 0 || max < 0) {
+    if (typeof min === 'string' || typeof max === 'string') {
+        summedNum = "ERROR";
+    } else if (min < 0 || max < 0) {
         summedNum = "ERROR";
     } else if (max > min) {
         for(let i = 0; i <= max; i++) {
