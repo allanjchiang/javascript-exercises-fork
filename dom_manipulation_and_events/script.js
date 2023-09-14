0370b31f37b34a3766d383dff46c2096721b9c48
Allan Chiang <allanchiang24@gmail.com>
2023-09-14 17:38:46 +1200

    Create div and h1 in memory with border, background color and textContent

--- dom_manipulation_and_events/script.js
+++ dom_manipulation_and_events/script.js
@@ -9,3 +9,9 @@ const h3 = document.createElement('h3');
 h3.textContent = 'I\'m a blue h3!';
 h3.style.color = 'blue';
 container.appendChild(h3);
+
+const div = document.createElement('div');
+div.style.cssText = 'border: black 1px solid; background-color: pink;';
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I\'m in a div!';
