e812fbf9750e03645f20bb0cc22fa86f4c6f54fa
Allan Chiang <allanchiang24@gmail.com>
2023-09-18 14:15:48 +1200

    Add event listener of click to btn2

--- dom_manipulation_and_events/button_alert_hello_world/script.js
+++ dom_manipulation_and_events/button_alert_hello_world/script.js
@@ -3,3 +3,7 @@ const button = document.querySelector('#btn');
 button.onclick = () => alert("Hello World");
 
 const btn2 = document.querySelector('#btn2');
+
+btn2.addEventListener('click', () => {
+    alert('Hello World!');
+});
