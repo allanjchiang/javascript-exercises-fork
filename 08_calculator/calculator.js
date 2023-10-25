fa2c1a3a05e2318c7615944f21f00792bf20840d
Allan <allanchiang24@gmail.com>
2023-10-25 16:26:05 +1300

    Add ...theArgs as parameter and add a for of loop to sum all numbers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,7 +6,12 @@ const subtract = function(number1, number2) {
 	return number1 - number2;
 };
 
-const sum = function() {
+const sum = function(...theArgs) {
+  let total = 0;
+  for (const arg of theArgs) {
+    total += arg;
+  }
+  return total;
 	
 };
 
